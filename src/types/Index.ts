a239abaf519385c4a90d8002a6818cf1a8b369f7
MeronTeweldebrhan <meronqelati@gmail.com>
2025-06-22 12:01:34 -0400

    Update Index.ts types

--- src/types/Index.ts
+++ src/types/Index.ts
@@ -1,41 +1,36 @@
-export type TaskStatus = 'pending' | 'in-progress' | 'completed';
- export type TaskPriority = "low" | "medium" | "high";
+export type TaskStatus = 'all'|'pending' | 'in-progress' | 'completed';
+ export type TaskPriority = 'all'| "low" | "medium" | "high";
 
  export interface TaskFormProps {
-  initialData?: {
-    title: string;
-    description: string;
-    dueDate: string;
-  };
-  onSubmit: (data: { title: string; description: string; dueDate: string }) => void;
-  onCancel?: () => void;
+   onAddTask: (task: Task) => void;
  }
 export interface Task {
   id: string;
   title: string;
   description: string;
-  status: TaskStatus;
-  priority: TaskPriority;
+  status?: TaskStatus;
+  priority?: TaskPriority;
   dueDate: string;
 }
  
 export interface TaskListProps {
   tasks: Task[];
- onUpdate: (task: Task) => void;
-  onPriorityChange?:(taskId:string,newPriority:TaskPriority) =>void
+  onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
+  onPriorityChange:(taskId:string,newPriority:TaskPriority) =>void
   onDelete: (taskId: string) => void;
 }
 
 export interface TaskItemProps {
   task: Task;
-  onUpdate:(task: Task) => void
-  onDelete: (taskid: string) => void;
+  onStatusChange: (taskId: string, newStatus: TaskStatus) => void;
+  onPriorityChange: (taskId: string, newPriority: TaskPriority) => void;
+  onDelete: (taskId: string) => void;
 }
 
 export interface TaskFilterProps {
-  onStatusFilterChange: (status: TaskStatus | "all") => void;
-  onPriorityFilterChange: (priority: TaskPriority | "all") => void;
-  currentStatus?: string; 
-  currentPriority?: string; 
+ onStatusFilterChange: (status: TaskStatus | "all") => void;
+ onPriorityFilterChange:(status: TaskPriority | "all") => void;
+ currentStatus?: string; 
+  currentPriority?: string;
 }
 
